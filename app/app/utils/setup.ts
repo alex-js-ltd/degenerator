cd31675e45190aa877e8da5f7a6e54718f75f9b8
alex-js-ltd <alex.js.ltd@gmail.com>
2024-05-31 19:43:22 +0200

    update

--- app/app/utils/setup.ts
+++ app/app/utils/setup.ts
@@ -1,14 +1,14 @@
 import { Program } from '@coral-xyz/anchor'
-import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js'
+import { clusterApiUrl, Connection } from '@solana/web3.js'
 
-import type { Metadata } from '@/app/types/metadata'
-import IDL from '@/app/idl/metadata.json'
+import type { Degenerator } from '@/app/types/degenerator'
+import IDL from '@/app/idl/degenerator.json'
 
 export const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
 
 // Initialize the program interface with the IDL, program ID, and connection.
 // This setup allows us to interact with the on-chain program using the defined interface.
 
-export const program = new Program<Metadata>(IDL as Metadata, {
+export const program = new Program<Degenerator>(IDL as Degenerator, {
 	connection,
 })
