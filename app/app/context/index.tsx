d3dad1d03a6f5e8af1c7cd687e7c9f4220968e3a
alex-js-ltd <alex.js.ltd@gmail.com>
2024-06-05 17:04:41 +0200

    update

--- app/app/context/index.tsx
+++ app/app/context/index.tsx
@@ -1,13 +1,8 @@
 'use client'
 
 import type { ReactNode } from 'react'
-import { NetworkProvider } from './network_context'
 import { WalletProvider } from './wallet_context'
 
 export function AppProviders({ children }: { children: ReactNode }) {
-	return (
-		<NetworkProvider>
-			<WalletProvider>{children}</WalletProvider>
-		</NetworkProvider>
-	)
+	return <WalletProvider>{children}</WalletProvider>
 }
--- app/app/context/wallet_context.tsx
+++ app/app/context/wallet_context.tsx
@@ -1,41 +1,30 @@
 'use client'
 
-import { useMemo, type ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import {
 	ConnectionProvider,
 	UnifiedWalletProvider,
-	WalletAdapterNetwork,
 } from '@jup-ag/wallet-adapter'
-import { useNetworkConfiguration } from './network_context'
+
 import { getEnv } from '@/app/utils/env'
-import { clusterApiUrl } from '@solana/web3.js'
 
 const { CLUSTER } = getEnv()
 
 export function WalletProvider({ children }: { children: ReactNode }) {
-	const { networkConfiguration } = useNetworkConfiguration()
-
-	const network = networkConfiguration as WalletAdapterNetwork
-
-	const selectedEndpoint: string = useMemo(() => CLUSTER, [network])
-
 	return (
-		<ConnectionProvider endpoint={selectedEndpoint}>
+		<ConnectionProvider endpoint={CLUSTER}>
 			<UnifiedWalletProvider
 				wallets={[]}
 				config={{
 					autoConnect: true,
 					env: 'mainnet-beta',
 					metadata: {
-						name: 'UnifiedWallet',
-						description: 'UnifiedWallet',
-						url: 'https://jup.ag',
-						iconUrls: ['https://jup.ag/favicon.ico'],
+						name: 'Degenerator',
+						description: 'Generate SPL Tokens on Solana',
+						url: 'https://www.degenerator.dev',
+						iconUrls: [],
 					},
 
-					walletlistExplanation: {
-						href: 'https://station.jup.ag/docs/additional-topics/wallet-list',
-					},
 					theme: 'light',
 					lang: 'en',
 				}}
