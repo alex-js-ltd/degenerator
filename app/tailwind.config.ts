52b9c5b2b1685a7e5b1fcb1d557448e28fd673ab
alex-js-ltd <alex.js.ltd@gmail.com>
2024-06-11 15:55:32 +0200

    update tailwind colors

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -13,7 +13,29 @@ const config: Config = {
 				'gradient-conic':
 					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
-	
+
+			colors: {
+				success: {
+					bg: 'hsl(150, 100%, 6%)',
+					border: 'hsl(147, 100%, 12%)',
+					text: 'hsl(150, 86%, 65%)',
+				},
+				info: {
+					bg: 'hsl(215, 100%, 6%)',
+					border: 'hsl(223, 100%, 12%)',
+					text: 'hsl(216, 87%, 65%)',
+				},
+				warning: {
+					bg: 'hsl(64, 100%, 6%)',
+					border: 'hsl(60, 100%, 12%)',
+					text: 'hsl(46, 87%, 65%)',
+				},
+				error: {
+					bg: 'hsl(358, 76%, 10%)',
+					border: 'hsl(357, 89%, 16%)',
+					text: 'hsl(358, 100%, 81%)',
+				},
+			},
 		},
 	},
 	plugins: [],
