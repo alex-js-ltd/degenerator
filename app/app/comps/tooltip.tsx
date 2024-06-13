7e71795beca7a5c5cc05cfbd864f557ef9e430c8
alex-js-ltd <alex.js.ltd@gmail.com>
2024-06-13 02:43:33 +0200

    update

--- app/app/comps/tooltip.tsx
+++ app/app/comps/tooltip.tsx
@@ -11,7 +11,7 @@ export function Tooltip({ open, children, content }: TooltipProps) {
 				<TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
 				<TooltipPrimitive.Portal>
 					<TooltipPrimitive.Content
-						className="z-50 bg-gray-900 shadow-lg  text-white data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade overflow-hidden rounded-md bg-primary px-3 py-2 text-sm text-primary-foreground leading-none will-change-[transform,opacity]"
+						className="z-50 bg-gray-900 shadow-lg  text-white data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade overflow-hidden rounded-md bg-primary px-[12px] h-[32px] text-sm flex items-center leading-none will-change-[transform,opacity]"
 						sideOffset={18}
 						align="end"
 						alignOffset={-12}
