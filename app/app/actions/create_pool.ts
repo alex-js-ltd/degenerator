b7540dc481acb983349b229c4b6b60ca5add3cf5
alex-js-ltd <alex.js.ltd@gmail.com>
2024-06-18 16:22:52 +0200

    update

--- app/app/actions/create_pool.ts
+++ app/app/actions/create_pool.ts
@@ -4,39 +4,84 @@ import { parseWithZod } from '@conform-to/zod'
 import { PoolSchema } from '@/app/utils/schemas'
 import { program, connection } from '@/app/utils/setup'
 import { getEnv } from '@/app/utils/env'
-import {
-	Raydium,
-	TxVersion,
-	parseTokenAccountResp,
-	OPEN_BOOK_PROGRAM,
-	DEVNET_PROGRAM_ID,
-	RAYMint,
-	USDCMint,
-} from '@raydium-io/raydium-sdk-v2'
 
-import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token'
-import { PublicKey } from '@solana/web3.js'
+import {
+	Connection,
+	Keypair,
+	PublicKey,
+	sendAndConfirmTransaction,
+	SYSVAR_CLOCK_PUBKEY,
+	ParsedAccountData,
+	Transaction,
+	VersionedTransaction,
+} from '@solana/web3.js'
+import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes'
+import DLMM, { BinLiquidity, LbPosition, StrategyType } from '@meteora-ag/dlmm'
+import BN from 'bn.js'
+
+import invariant from 'tiny-invariant'
 
 const { CLUSTER } = getEnv()
-const txVersion = TxVersion.V0 // or TxVersion.LEGACY
 
-const fetchTokenAccountData = async (owner: PublicKey) => {
-	const solAccountResp = await connection.getAccountInfo(owner)
-	const tokenAccountResp = await connection.getTokenAccountsByOwner(owner, {
-		programId: TOKEN_PROGRAM_ID,
-	})
-	const token2022Req = await connection.getTokenAccountsByOwner(owner, {
-		programId: TOKEN_2022_PROGRAM_ID,
-	})
-	const tokenAccountData = parseTokenAccountResp({
-		owner: owner,
-		solAccountResp,
-		tokenAccountResp: {
-			context: tokenAccountResp.context,
-			value: [...tokenAccountResp.value, ...token2022Req.value],
-		},
-	})
-	return tokenAccountData
+const devnetPool = new PublicKey('3W2HKgUa96Z69zzG3LK1g8KdcRAWzAttiLiHfYnKuPw5')
+
+/** Utils */
+export interface ParsedClockState {
+	info: {
+		epoch: number
+		epochStartTimestamp: number
+		leaderScheduleEpoch: number
+		slot: number
+		unixTimestamp: number
+	}
+	type: string
+	program: string
+	space: number
+}
+
+let activeBin: BinLiquidity
+let userPositions: LbPosition[] = []
+
+const newBalancePosition = new Keypair()
+const newImbalancePosition = new Keypair()
+const newOneSidePosition = new Keypair()
+
+async function getActiveBin(dlmmPool: DLMM) {
+	// Get pool state
+	activeBin = await dlmmPool.getActiveBin()
+	console.log('🚀 ~ activeBin:', activeBin)
+}
+
+// To create a balance deposit position
+async function createBalancePosition(
+	dlmmPool: DLMM,
+	user: { publicKey: PublicKey },
+) {
+	const TOTAL_RANGE_INTERVAL = 10 // 10 bins on each side of the active bin
+	const minBinId = activeBin.binId - TOTAL_RANGE_INTERVAL
+	const maxBinId = activeBin.binId + TOTAL_RANGE_INTERVAL
+
+	const activeBinPricePerToken = dlmmPool.fromPricePerLamport(
+		Number(activeBin.price),
+	)
+	const totalXAmount = new BN(100)
+	const totalYAmount = totalXAmount.mul(new BN(Number(activeBinPricePerToken)))
+
+	// Create Position
+	const createPositionTx =
+		await dlmmPool.initializePositionAndAddLiquidityByStrategy({
+			positionPubKey: newBalancePosition.publicKey,
+			user: user.publicKey,
+			totalXAmount,
+			totalYAmount,
+			strategy: {
+				maxBinId,
+				minBinId,
+				strategyType: StrategyType.SpotBalanced,
+			},
+		})
+
+	return createPositionTx
 }
 
 export async function createPool(_prevState: unknown, formData: FormData) {
@@ -47,79 +92,49 @@ export async function createPool(_prevState: unknown, formData: FormData) {
 	if (submission.status !== 'success') {
 		return {
 			...submission.reply(),
-			serializedTransactions: undefined,
+			encoded_transaction: undefined,
 		}
 	}
 
 	const { owner } = submission.value
 
-	let raydium: Raydium | undefined
-	const initSdk = async (params?: { loadToken?: boolean }) => {
-		if (raydium) return raydium
-		raydium = await Raydium.load({
-			owner,
-			connection,
-			cluster: 'devnet', // 'mainnet' | 'devnet'
-			disableFeatureCheck: true,
-			disableLoadToken: !params?.loadToken,
-			blockhashCommitment: 'finalized',
-			// urlConfigs: {
-			//   BASE_HOST: '<API_HOST>', // api url configs, currently api doesn't support devnet
-			// },
-		})
+	const dlmmPool = await DLMM.create(connection, devnetPool, {
+		cluster: 'devnet',
+	})
 
-		return raydium
-	}
+	await getActiveBin(dlmmPool)
+	const transaction = await createBalancePosition(dlmmPool, {
+		publicKey: owner,
+	})
 
-	raydium = await initSdk()
+	const blockHash = (await connection.getLatestBlockhash('finalized')).blockhash
+	transaction.feePayer = owner
+	transaction.recentBlockhash = blockHash
+	const serializedTransaction = transaction.serialize({
+		requireAllSignatures: false,
+		verifySignatures: true,
+	})
 
-	console.log(raydium)
-
-	const TEST_MINT = new PublicKey(
-		'FotFQ4muw3vY3ZuyaMjpZYM1bCgiFJ7aDs3ZgXva2V2Y',
-	)
-
-	const { execute, extInfo, transactions, builder, signers, ...rest } =
-		await raydium.marketV2.create({
-			baseInfo: {
-				mint: TEST_MINT,
-				decimals: 4,
-			},
-			quoteInfo: {
-				mint: USDCMint,
-				decimals: 9,
-			},
-			lotSize: 1,
-			tickSize: 0.01,
-			dexProgramId: DEVNET_PROGRAM_ID.OPENBOOK_MARKET, // devnet: DEVNET_PROGRAM_ID.OPENBOOK_MARKET
-			txVersion,
-			// optional: set up priority fee here
-			// computeBudgetConfig: {
-			//   units: 600000,
-			//   microLamports: 100000000,
-			// },
-		})
-
-	console.log(
-		`create market total ${transactions.length} txs, market info: `,
-		Object.keys(extInfo.address).reduce(
-			(acc, cur) => ({
-				...acc,
-				[cur]: extInfo.address[cur as keyof typeof extInfo.address].toBase58(),
-			}),
-			{},
-		),
-	)
-
-	// const txIds = await execute({
-	// 	// set sequentially to true means tx will be sent when previous one confirmed
-	// 	sequentially: true,
-	// })
-
-	const serializedTransactions = transactions.map(el => el.serialize())
+	const transactionBase64 = serializedTransaction.toString('base64')
 
 	return {
 		...submission.reply(),
-		serializedTransactions,
+		encoded_transaction: transactionBase64,
 	}
+}
+
+function getRawTransaction(
+	encodedTransaction: string,
+): Transaction | VersionedTransaction {
+	let recoveredTransaction: Transaction | VersionedTransaction
+	try {
+		recoveredTransaction = Transaction.from(
+			Buffer.from(encodedTransaction, 'base64'),
+		)
+	} catch (error) {
+		recoveredTransaction = VersionedTransaction.deserialize(
+			Buffer.from(encodedTransaction, 'base64'),
+		)
+	}
+	return recoveredTransaction
 }
