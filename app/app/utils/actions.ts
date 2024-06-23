f2ce128688c3e475cfbf5738254106a3bdc8c45e
alex-js-ltd <alex.js.ltd@gmail.com>
2024-06-23 16:35:19 +0200

    avoid mutation

--- app/app/utils/actions.ts
+++ app/app/utils/actions.ts
@@ -6,9 +6,13 @@ import { put } from '@vercel/blob'
 import { prisma } from '@/app/utils/db'
 import invariant from 'tiny-invariant'
 import { program, connection } from '@/app/utils/setup'
-import { TransactionMessage, VersionedTransaction } from '@solana/web3.js'
+import {
+	PublicKey,
+	TransactionMessage,
+	VersionedTransaction,
+} from '@solana/web3.js'
 import { TOKEN_2022_PROGRAM_ID } from '@solana/spl-token'
-import * as anchor from '@coral-xyz/anchor'
+import { web3, BN } from '@coral-xyz/anchor'
 
 export async function createSplToken(_prevState: unknown, formData: FormData) {
 	const submission = parseWithZod(formData, {
@@ -55,93 +59,23 @@ export async function createSplToken(_prevState: unknown, formData: FormData) {
 		uri: `https://degenerator-tawny.vercel.app/api/metadata/${upload.id}`,
 	}
 
-	const mintKeypair = new anchor.web3.Keypair()
+	const mintKeypair = new web3.Keypair()
 
-	const ATA_PROGRAM_ID = new anchor.web3.PublicKey(
-		'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+	const mintKey = mintKeypair.publicKey
+
+	const instructions = await getInstructions(
+		payerKey,
+		mintKey,
+		metadata,
+		decimals,
+		supply,
+		revokeMint,
+		revokeFreeze,
 	)
-
-	const [receiverATA] = anchor.web3.PublicKey.findProgramAddressSync(
-		[
-			payerKey.toBytes(),
-			TOKEN_2022_PROGRAM_ID.toBytes(),
-			mintKeypair.publicKey.toBytes(),
-		],
-		ATA_PROGRAM_ID,
-	)
-
-	// Create Mint with MetadataPointer and TokenMetadata Extensions
-	const initialize = await program.methods
-		.initialize(metadata, decimals)
-		.accounts({ mintAccount: mintKeypair.publicKey, payer: payerKey })
-		.instruction()
-
-	invariant(initialize, 'Failed to create mint')
-
-	// Create Associated Token Account
-	const createAssociatedTokenAccount = await program.methods
-		.createAssociatedTokenAccount()
-		.accounts({
-			tokenAccount: receiverATA,
-			mint: mintKeypair.publicKey,
-			signer: payerKey,
-			tokenProgram: TOKEN_2022_PROGRAM_ID,
-		})
-		.instruction()
-
-	invariant(
-		createAssociatedTokenAccount,
-		'Failed to create associated token account',
-	)
-
-	// Mint Token to Payer
-	const mintToken = await program.methods
-		.mintToken(new anchor.BN(supply))
-		.accounts({
-			mint: mintKeypair.publicKey,
-			signer: payerKey,
-			receiver: receiverATA,
-			tokenProgram: TOKEN_2022_PROGRAM_ID,
-		})
-		.instruction()
-
-	invariant(mintToken, 'Failed to mint token')
-
-	// Revoke Freeze Authority
-	const revokeFreezeAuthority = await program.methods
-		.revokeFreezeAuthority()
-		.accounts({
-			currentAuthority: payerKey,
-			mintAccount: mintKeypair.publicKey,
-		})
-		.instruction()
-
-	invariant(revokeFreezeAuthority, 'Failed to revoke freeze authority')
-
-	// Close Mint
-	const revokeMintAuthority = await program.methods
-		.revokeMintAuthority()
-		.accounts({
-			currentAuthority: payerKey,
-			mintAccount: mintKeypair.publicKey,
-		})
-		.instruction()
-
-	invariant(revokeMintAuthority, 'Failed to revoke mint authority')
 
 	let blockhash = await connection
 		.getLatestBlockhash()
 		.then(res => res.blockhash)
-
-	const instructions = [initialize, createAssociatedTokenAccount, mintToken]
-
-	if (revokeFreeze) {
-		instructions.push(revokeFreezeAuthority)
-	}
-
-	if (revokeMint) {
-		instructions.push(revokeMintAuthority)
-	}
 
 	const messageV0 = new TransactionMessage({
 		payerKey,
@@ -159,4 +93,112 @@ export async function createSplToken(_prevState: unknown, formData: FormData) {
 		...submission.reply(),
 		serializedTransaction,
 	}
+}
+
+async function getInstructions(
+	payerKey: PublicKey,
+	mintKey: PublicKey,
+	metadata: { name: string; symbol: string; uri: string },
+	decimals: number,
+	supply: number,
+	revokeMint?: boolean,
+	revokeFreeze?: boolean,
+) {
+	const ATA_PROGRAM_ID = new web3.PublicKey(
+		'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+	)
+
+	const [receiverATA] = web3.PublicKey.findProgramAddressSync(
+		[payerKey.toBytes(), TOKEN_2022_PROGRAM_ID.toBytes(), mintKey.toBytes()],
+		ATA_PROGRAM_ID,
+	)
+
+	// Create Mint with MetadataPointer and TokenMetadata Extensions
+	const initialize = await program.methods
+		.initialize(metadata, decimals)
+		.accounts({ mintAccount: mintKey, payer: payerKey })
+		.instruction()
+
+	invariant(initialize, 'Failed to create mint')
+
+	// Create Associated Token Account
+	const createAta = await program.methods
+		.createAssociatedTokenAccount()
+		.accounts({
+			tokenAccount: receiverATA,
+			mint: mintKey,
+			signer: payerKey,
+			tokenProgram: TOKEN_2022_PROGRAM_ID,
+		})
+		.instruction()
+
+	invariant(createAta, 'Failed to create associated token account')
+
+	// Mint Token to Payer
+	const mintToken = await program.methods
+		.mintToken(new BN(supply))
+		.accounts({
+			mint: mintKey,
+			signer: payerKey,
+			receiver: receiverATA,
+			tokenProgram: TOKEN_2022_PROGRAM_ID,
+		})
+		.instruction()
+
+	invariant(mintToken, 'Failed to mint token')
+
+	// additional instructions
+	const revokeMintAuth =
+		revokeMint && (await getRevokeMintAuth(payerKey, mintKey))
+	const revokeFreezeAuth =
+		revokeFreeze && (await getFreezeAuth(payerKey, mintKey))
+
+	const instructions = [
+		initialize,
+		createAta,
+		mintToken,
+		revokeMintAuth,
+		revokeFreezeAuth,
+	]
+
+	const initial: web3.TransactionInstruction[] = []
+
+	return instructions.reduce((acc, curr) => {
+		if (curr) acc.push(curr)
+		return acc
+	}, initial)
+}
+
+async function getRevokeMintAuth(
+	currentAuthority: PublicKey,
+	mintAccount: PublicKey,
+) {
+	const revokeMintAuthority = await program.methods
+		.revokeMintAuthority()
+		.accounts({
+			currentAuthority,
+			mintAccount,
+		})
+		.instruction()
+
+	invariant(revokeMintAuthority, 'Failed to revoke mint authority')
+
+	return revokeMintAuthority
+}
+
+async function getFreezeAuth(
+	currentAuthority: PublicKey,
+	mintAccount: PublicKey,
+) {
+	const revokeFreezeAuthority = await program.methods
+		.revokeFreezeAuthority()
+		.accounts({
+			currentAuthority,
+			mintAccount,
+		})
+		.instruction()
+
+	invariant(revokeFreezeAuthority, 'Failed to revoke freeze authority')
+
+	return revokeFreezeAuthority
 }
