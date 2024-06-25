6d8c16f8e1b08e2efd5cdcd37bca3ded58428db9
alex-js-ltd <alex.js.ltd@gmail.com>
2024-06-25 12:20:50 +0200

    update

--- app/app/utils/actions.ts
+++ app/app/utils/actions.ts
@@ -5,14 +5,10 @@ import { TokenSchema } from './schemas'
 import { put } from '@vercel/blob'
 import { prisma } from '@/app/utils/db'
 import invariant from 'tiny-invariant'
-import { program, connection } from '@/app/utils/setup'
-import {
-	PublicKey,
-	TransactionMessage,
-	VersionedTransaction,
-} from '@solana/web3.js'
-import { TOKEN_2022_PROGRAM_ID } from '@solana/spl-token'
-import { web3, BN } from '@coral-xyz/anchor'
+import { connection } from '@/app/utils/setup'
+import { TransactionMessage, VersionedTransaction } from '@solana/web3.js'
+import { web3 } from '@coral-xyz/anchor'
+import { getInstructions } from '@/app/utils/instructions'
 
 export async function createSplToken(_prevState: unknown, formData: FormData) {
 	const submission = parseWithZod(formData, {
@@ -63,7 +59,7 @@ export async function createSplToken(_prevState: unknown, formData: FormData) {
 
 	const mintKey = mintKeypair.publicKey
 
-	const instructions = await getInstructions(
+	const instructions = await getInstructions({
 		payerKey,
 		mintKey,
 		metadata,
@@ -71,7 +67,7 @@ export async function createSplToken(_prevState: unknown, formData: FormData) {
 		supply,
 		revokeMint,
 		revokeFreeze,
-	)
+	})
 
 	let blockhash = await connection
 		.getLatestBlockhash()
@@ -93,112 +89,4 @@ export async function createSplToken(_prevState: unknown, formData: FormData) {
 		...submission.reply(),
 		serializedTransaction,
 	}
-}
-
-async function getInstructions(
-	payerKey: PublicKey,
-	mintKey: PublicKey,
-	metadata: { name: string; symbol: string; uri: string },
-	decimals: number,
-	supply: number,
-	revokeMint?: boolean,
-	revokeFreeze?: boolean,
-) {
-	const ATA_PROGRAM_ID = new web3.PublicKey(
-		'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
-	)
-
-	const [receiverATA] = web3.PublicKey.findProgramAddressSync(
-		[payerKey.toBytes(), TOKEN_2022_PROGRAM_ID.toBytes(), mintKey.toBytes()],
-		ATA_PROGRAM_ID,
-	)
-
-	// Create Mint with MetadataPointer and TokenMetadata Extensions
-	const initialize = await program.methods
-		.initialize(metadata, decimals)
-		.accounts({ mintAccount: mintKey, payer: payerKey })
-		.instruction()
-
-	invariant(initialize, 'Failed to create mint')
-
-	// Create Associated Token Account
-	const createAta = await program.methods
-		.createAssociatedTokenAccount()
-		.accounts({
-			tokenAccount: receiverATA,
-			mint: mintKey,
-			signer: payerKey,
-			tokenProgram: TOKEN_2022_PROGRAM_ID,
-		})
-		.instruction()
-
-	invariant(createAta, 'Failed to create associated token account')
-
-	// Mint Token to Payer
-	const mintToken = await program.methods
-		.mintToken(new BN(supply))
-		.accounts({
-			mint: mintKey,
-			signer: payerKey,
-			receiver: receiverATA,
-			tokenProgram: TOKEN_2022_PROGRAM_ID,
-		})
-		.instruction()
-
-	invariant(mintToken, 'Failed to mint token')
-
-	// additional instructions
-	const revokeMintAuth =
-		revokeMint && (await getRevokeMintAuth(payerKey, mintKey))
-	const revokeFreezeAuth =
-		revokeFreeze && (await getFreezeAuth(payerKey, mintKey))
-
-	const instructions = [
-		initialize,
-		createAta,
-		mintToken,
-		revokeMintAuth,
-		revokeFreezeAuth,
-	]
-
-	const initial: web3.TransactionInstruction[] = []
-
-	return instructions.reduce((acc, curr) => {
-		if (curr) acc.push(curr)
-		return acc
-	}, initial)
-}
-
-async function getRevokeMintAuth(
-	currentAuthority: PublicKey,
-	mintAccount: PublicKey,
-) {
-	const revokeMintAuthority = await program.methods
-		.revokeMintAuthority()
-		.accounts({
-			currentAuthority,
-			mintAccount,
-		})
-		.instruction()
-
-	invariant(revokeMintAuthority, 'Failed to revoke mint authority')
-
-	return revokeMintAuthority
-}
-
-async function getFreezeAuth(
-	currentAuthority: PublicKey,
-	mintAccount: PublicKey,
-) {
-	const revokeFreezeAuthority = await program.methods
-		.revokeFreezeAuthority()
-		.accounts({
-			currentAuthority,
-			mintAccount,
-		})
-		.instruction()
-
-	invariant(revokeFreezeAuthority, 'Failed to revoke freeze authority')
-
-	return revokeFreezeAuthority
 }
