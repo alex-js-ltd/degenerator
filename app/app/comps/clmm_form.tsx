f21d623c7c1afb2f61efe5b5b4b5460577cd6c3b
alex-js-ltd <alex.js.ltd@gmail.com>
2024-07-08 22:57:15 +0100

    update

--- app/app/comps/clmm_form.tsx
+++ app/app/comps/clmm_form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Fragment, useEffect, useRef } from 'react'
+import { Fragment, useEffect } from 'react'
 import {
 	useForm,
 	getFormProps,
@@ -11,21 +11,13 @@ import { parseWithZod } from '@conform-to/zod'
 
 import { ClmmSchema } from '@/app/utils/schemas'
 import { Input } from '@/app/comps/input'
-import { useFormState } from 'react-dom'
 import { useAsync } from '@/app/hooks/use_async'
-import { useSignAndSendTransaction } from '@/app/hooks/use_sign_and_send_transaction'
-import { useSerializedTransaction } from '@/app/hooks/use_serialized_transaction'
 import { usePayer } from '@/app/hooks/use_payer'
 import { Toast, getSuccessProps, getErrorProps } from '@/app/comps/toast'
 import { type SelectItemConfig, QuoteToken, FeeTier } from '@/app/comps/select'
-import { clmm } from '@/app/actions/clmm'
 import { SubmitButton } from '@/app/comps/submit_button'
 import { useRaydium } from '@/app/hooks/use_raydium'
 import { useClmm } from '@/app/hooks/use_clmm'
-
-const initialState = {
-	serializedTransaction: undefined,
-}
 
 export function ClmmForm({
 	mintItems,
@@ -36,8 +28,6 @@ export function ClmmForm({
 	clmmItems: SelectItemConfig[]
 	mint1?: string
 }) {
-	const [lastResult, action] = useFormState(clmm, initialState)
-
 	const [form, fields] = useForm({
 		onValidate({ formData }) {
 			return parseWithZod(formData, { schema: ClmmSchema })
@@ -47,9 +37,6 @@ export function ClmmForm({
 	})
 
 	const payer = usePayer()
-	const { serializedTransaction } = lastResult
-	const transaction = useSerializedTransaction({ serializedTransaction })
-	const signAndSendTransaction = useSignAndSendTransaction()
 
 	const {
 		run,
@@ -60,10 +47,6 @@ export function ClmmForm({
 		error,
 		reset,
 	} = useAsync<string>()
-
-	useEffect(() => {
-		if (transaction) run(signAndSendTransaction(transaction))
-	}, [run, signAndSendTransaction, transaction])
 
 	const initSdk = useRaydium()
 
