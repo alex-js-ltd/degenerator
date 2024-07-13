a5e4de15ef4b4cc40a35851c16a8c448e42209d1
alex-js-ltd <alex.js.ltd@gmail.com>
2024-07-14 00:37:31 +0100

    update

--- app/app/utils/misc.ts
+++ app/app/utils/misc.ts
@@ -63,19 +63,64 @@ export function callAll<Args extends Array<unknown>>(
 	return (...args: Args) => fns.forEach(fn => fn?.(...args))
 }
 
-// const asyncPipe =
-//   (...functions) =>
-//   (initialValue) =>
-//     functions.reduce(async (acc, fn) => fn(await acc), initialValue);
+type Func<T extends any[], R> = (...a: T) => R
 
-function asyncPipe<Args extends any[]>(
-	initialValue: any,
-	...fns: Array<((...args: Args) => Promise<any>) | undefined>
-) {
-	return fns.reduce(async (acc, fn) => {
-		await acc
-		if (fn) return fn(...acc) // Spread acc to match Args for fn
+/**
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for the
+ * resulting composite function.
+ *
+ * @param funcs The functions to compose.
+ * @returns A function obtained by composing the argument functions from right
+ *   to left. For example, `compose(f, g, h)` is identical to doing
+ *   `(...args) => f(g(h(...args)))`.
+ */
+export function compose(): <R>(a: R) => R
 
-		return acc
-	}, initialValue)
+export function compose<F extends Function>(f: F): F
+
+/* two functions */
+export function compose<A, T extends any[], R>(
+	f1: (a: A) => R,
+	f2: Func<T, A>,
+): Func<T, R>
+
+/* three functions */
+export function compose<A, B, T extends any[], R>(
+	f1: (b: B) => R,
+	f2: (a: A) => B,
+	f3: Func<T, A>,
+): Func<T, R>
+
+/* four functions */
+export function compose<A, B, C, T extends any[], R>(
+	f1: (c: C) => R,
+	f2: (b: B) => C,
+	f3: (a: A) => B,
+	f4: Func<T, A>,
+): Func<T, R>
+
+/* rest */
+export function compose<R>(
+	f1: (a: any) => R,
+	...funcs: Function[]
+): (...args: any[]) => R
+
+export function compose<R>(...funcs: Function[]): (...args: any[]) => R
+
+export function compose(...funcs: Function[]) {
+	if (funcs.length === 0) {
+		// infer the argument type so it is usable in inference down the line
+		return <T>(arg: T) => arg
+	}
+
+	if (funcs.length === 1) {
+		return funcs[0]
+	}
+
+	return funcs.reduce(
+		(a, b) =>
+			(...args: any) =>
+				a(b(...args)),
+	)
 }
