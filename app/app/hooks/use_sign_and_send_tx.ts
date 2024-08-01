b64ad7a47f46fb4d2e5f11a8dcb2feea62be9876
alex-js-ltd <alex.js.ltd@gmail.com>
2024-08-01 22:35:40 +0100

    update

--- app/app/hooks/use_sign_and_send_tx.ts
+++ app/app/hooks/use_sign_and_send_tx.ts
@@ -14,7 +14,7 @@ export function useSignAndSendTx() {
 
 			const blocks = connection.getLatestBlockhash()
 			const send = sendTransaction(tx, connection, {
-				skipPreflight: true,
+				skipPreflight: false,
 			})
 
 			const [latestBlockhash, signature] = await Promise.all([blocks, send])
