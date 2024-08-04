3aec3f4ffe4f30223d79fbc8c7dda0c491cde39a
alex-js-ltd <alex.js.ltd@gmail.com>
2024-08-04 17:41:09 +0100

    add mint list

--- app/app/hooks/use_selected_item.ts
+++ app/app/hooks/use_selected_item.ts
@@ -1,12 +1,14 @@
 import { type SelectItemConfig } from '@/app/comps/select'
 import { type FieldName, useField } from '@conform-to/react'
+import { useMintList } from './use_mint_list'
 
 export function useSelectedItem(
 	name: FieldName<string>,
 	items: SelectItemConfig[],
 ) {
 	const [meta] = useField<string>(name)
-	const selected = items.find(el => el.value === meta.value)
 
+	const mintList = useMintList({ items })
+	const selected = mintList.find(el => el.value === meta.value)
 	return { meta, title: selected?.title, imageProps: selected?.imageProps }
 }
