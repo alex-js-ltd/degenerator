b96018c1ced2b95ceb688d4144fe79f23bcc10d8
alex-js-ltd <alex.js.ltd@gmail.com>
2024-08-04 23:13:30 +0100

    update

--- app/app/context/wallet_context.tsx
+++ app/app/context/wallet_context.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { type ReactNode, useMemo } from 'react'
+import { type ReactNode } from 'react'
 import { UnifiedWalletProvider } from '@jup-ag/wallet-adapter'
-
 import { getEnv } from '@/app/utils/env'
 
 const { CLUSTER } = getEnv()
@@ -11,34 +10,31 @@ const noop = () => {}
 
 type Params = Omit<Parameters<typeof UnifiedWalletProvider>[0], 'children'>
 
-export function WalletProvider({ children }: { children: ReactNode }) {
-	const params: Params = useMemo(
-		() => ({
-			wallets: [],
-			config: {
-				autoConnect: true,
-				env: CLUSTER,
-				metadata: {
-					name: 'Degenerator',
-					description: 'Generate SPL Tokens on Solana',
-					url: 'https://www.degenerator.dev',
-					iconUrls: ['https://www.degenerator.dev/favicon.ico'],
-				},
-				notificationCallback: {
-					onConnect: noop,
-					onConnecting: noop,
-					onDisconnect: noop,
-					onNotInstalled: noop,
-				},
-				walletlistExplanation: {
-					href: 'https://station.jup.ag/docs/additional-topics/wallet-list',
-				},
-				theme: 'light',
-				lang: 'en',
-			},
-		}),
-		[],
-	)
+const params: Params = {
+	wallets: [],
+	config: {
+		autoConnect: true,
+		env: CLUSTER,
+		metadata: {
+			name: 'Degenerator',
+			description: 'Generate SPL Tokens on Solana',
+			url: 'https://www.degenerator.dev',
+			iconUrls: ['https://www.degenerator.dev/favicon.ico'],
+		},
+		notificationCallback: {
+			onConnect: noop,
+			onConnecting: noop,
+			onDisconnect: noop,
+			onNotInstalled: noop,
+		},
+		walletlistExplanation: {
+			href: 'https://station.jup.ag/docs/additional-topics/wallet-list',
+		},
+		theme: 'light',
+		lang: 'en',
+	},
+}
 
+export function WalletProvider({ children }: { children: ReactNode }) {
 	return <UnifiedWalletProvider {...params}>{children}</UnifiedWalletProvider>
 }
