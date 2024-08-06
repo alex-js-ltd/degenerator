54b069a9fbba6b9d61cdb466544344755d90bc47
alex-js-ltd <alex.js.ltd@gmail.com>
2024-08-06 18:05:14 +0100

    add twitter link to footer

--- app/app/comps/footer.tsx
+++ app/app/comps/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
 		<footer className="fixed bottom-0 right-0 z-50 items-center justify-between px-4 pt-1 text-white mix-blend-difference hidden sm:inline-flex pb-2">
 			<nav className="flex items-center gap-2.5 rounded-full text-xs font-medium">
 				<Link
-					href={`https://x.com/home`}
+					href={`https://x.com/degenerato2774`}
 					target="_blank"
 					rel="noopener noreferrer"
 				>
