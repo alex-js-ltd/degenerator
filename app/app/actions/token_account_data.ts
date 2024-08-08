b82b81f6d0c4088974dad2c698f70b22fd391808
alex-js-ltd <alex.js.ltd@gmail.com>
2024-08-08 23:22:21 +0100

    add use server

--- app/app/actions/token_account_data.ts
+++ app/app/actions/token_account_data.ts
@@ -1,3 +1,5 @@
+'use server'
+
 import { parseTokenAccountResp } from '@raydium-io/raydium-sdk-v2'
 import { PublicKey } from '@solana/web3.js'
 import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token'
