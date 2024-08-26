f096d502e7a75cba37d218e6a82029ef74249fd2
alex-js-ltd <alex.js.ltd@gmail.com>
2024-08-26 21:16:45 +0100

    update cache

--- apps/web/app/data/featured_tokens.ts
+++ apps/web/app/data/featured_tokens.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/app/utils/db'
-import { unstable_cache } from 'next/cache'
+import { cache } from 'react'
 import 'server-only'
 
 async function fetchFeaturedTokens() {
@@ -10,13 +10,9 @@ async function fetchFeaturedTokens() {
 	return { data: tokens }
 }
 
-export const getFeaturedTokens = unstable_cache(
-	async () => {
-		const tokens = await fetchFeaturedTokens()
-		return tokens
-	},
-	['tokens'],
-	{ revalidate: 3600, tags: ['tokens'] },
-)
+export const getFeaturedTokens = cache(async () => {
+	const tokens = await fetchFeaturedTokens()
+	return tokens
+})
 
 export { fetchFeaturedTokens }
