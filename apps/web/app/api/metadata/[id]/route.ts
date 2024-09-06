5d6d019dbda05d69328e8006464b4d229ae14756
alex-js-ltd <alex.js.ltd@gmail.com>
2024-09-06 18:07:40 +0100

    get rid of delete api

--- apps/web/app/api/metadata/[id]/route.ts
+++ apps/web/app/api/metadata/[id]/route.ts
@@ -1,7 +1,6 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/app/utils/db'
 import { invariantResponse } from '@/app/utils/misc'
-import { auth } from '@/auth'
 
 export async function GET(
 	_request: NextRequest,
@@ -12,31 +11,6 @@ export async function GET(
 	const token = await prisma.tokenMetadata.findUnique({
 		where: {
 			id: params.id,
-		},
-	})
-
-	invariantResponse(token, 'failed to retrieve token metadata', { status: 500 })
-
-	const { id, createdAt, updatedAt, ownerId, ...meta } = token
-
-	return NextResponse.json({ ...meta })
-}
-
-export async function DELETE(
-	request: NextRequest,
-	{ params }: { params: { id: string } },
-) {
-	invariantResponse(params.id, 'missing id', { status: 404 })
-
-	console.log(request)
-	const session = await auth()
-
-	invariantResponse(session, 'unauthorised', { status: 403 })
-
-	const token = await prisma.tokenMetadata.delete({
-		where: {
-			id: params.id,
-			ownerId: session?.user?.id,
 		},
 	})
 
