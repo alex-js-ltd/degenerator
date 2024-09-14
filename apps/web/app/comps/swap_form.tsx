749e9813403c185ef52af0694e52c80b72462acb
alex-js-ltd <alex.js.ltd@gmail.com>
2024-09-14 05:10:57 +0100

    update

--- apps/web/app/comps/swap_form.tsx
+++ apps/web/app/comps/swap_form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode, useActionState, useEffect } from 'react'
+import { ReactNode, useActionState } from 'react'
 
 import { useForm, getFormProps, FormProvider } from '@conform-to/react'
 import { parseWithZod } from '@conform-to/zod'
@@ -12,6 +12,29 @@ import { SwapButton } from '@/app/comps/swap_button'
 import { useInputControl } from '@conform-to/react'
 import { getControlProps } from '@/app/utils/misc'
 import { SwapSwitch } from '@/app/comps/swap_switch'
+import { usePricePerToken } from '@/app/hooks/use_price_per_token'
+import { BN } from '@coral-xyz/anchor'
+import { LAMPORTS_PER_SOL } from '@solana/web3.js'
+
+function getTotalPrice(pricePerToken?: BN, amount?: string): string {
+	if (!pricePerToken || !amount) return '0'
+
+	const amountBN = new BN(amount || 0)
+	const totalPriceInLamports = pricePerToken.mul(amountBN) // Total price in lamports
+
+	// Divide the total lamports by LAMPORTS_PER_SOL (BN) to get the price in SOL (as BN with integer precision)
+	const LAMPORTS_PER_SOL_BN = new BN(LAMPORTS_PER_SOL)
+	const totalPriceInSOLBN = totalPriceInLamports.div(LAMPORTS_PER_SOL_BN)
+
+	// Get the remainder (for fractional SOL) and divide to add decimal precision
+	const remainder = totalPriceInLamports.mod(LAMPORTS_PER_SOL_BN)
+	const fractionalSOL = remainder.toNumber() / LAMPORTS_PER_SOL
+
+	// Return the total price in SOL, combining the integer part and fractional part
+	return (
+		totalPriceInSOLBN.toString() + '.' + fractionalSOL.toFixed(9).split('.')[1]
+	)
+}
 
 const initialState: State = {
 	lastResult: undefined,
@@ -46,6 +69,8 @@ export function SwapForm({ mint, token }: { mint: string; token: ReactNode }) {
 
 	const control = useInputControl(fields.amount)
 
+	const res = usePricePerToken(mint)
+
 	return (
 		<FormProvider context={form.context}>
 			<div className="rounded-b-xl w-full sm:max-w-xl">
@@ -56,6 +81,9 @@ export function SwapForm({ mint, token }: { mint: string; token: ReactNode }) {
 						formAction(formData)
 					}}
 				>
+					<span className="absolute top-3 right-3 z-50 text-teal-300 text-xs">
+						{`${getTotalPrice(res.data?.pricePerToken, control.value)} SOL`}
+					</span>
 					<input name="payer" defaultValue={payer} type="hidden" />
 					<input name="mint" defaultValue={mint} type="hidden" />
 					<div className="relative z-10 grid rounded-xl bg-white">
@@ -63,6 +91,7 @@ export function SwapForm({ mint, token }: { mint: string; token: ReactNode }) {
 							Swap input
 						</label>
 						<input
+							type="number"
 							placeholder="Amount…"
 							className="resize-none overflow-auto w-full flex-1 bg-transparent p-3 pb-1.5 text-sm outline-none ring-0"
 							style={{ minHeight: '42px', maxHeight: '384px', height: '42px' }}
