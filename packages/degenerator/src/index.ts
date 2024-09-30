daed745ac0c6b5b0edf81821df533cbc4578f956
alex-js-ltd <alex.js.ltd@gmail.com>
2024-10-01 00:03:16 +0100

    update

--- packages/degenerator/src/index.ts
+++ packages/degenerator/src/index.ts
@@ -1,14 +1,5 @@
-// Export everything from web3
 export * from './web3'
-
-// Export everything from pda
 export * from './pda'
-
-// Export everything from instruction
 export * from './instruction'
-
-// Export default IDL from target/idl
 export { default as IDL } from '../target/idl/degenerator.json'
-
-// Export type Degenerator
 export type { Degenerator } from '../target/types/degenerator'
