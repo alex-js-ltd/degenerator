f384604246f77036c1de67e4c3e2736877ee8215
alex-js-ltd <alex.js.ltd@gmail.com>
2024-10-23 21:20:38 +0100

    update use swap event

--- apps/web/app/hooks/use_swap_event.ts
+++ apps/web/app/hooks/use_swap_event.ts
@@ -1,33 +1,16 @@
-import { program } from '@/app/utils/setup'
-import { useEffect, experimental_useEffectEvent } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useRouter } from 'next/navigation'
 import { PublicKey } from '@solana/web3.js'
-import { useEventCallback } from 'usehooks-ts'
-
-interface Event {
-	mint: PublicKey
-}
+import { connection } from '@/app/utils/setup'
 
 export function useSwapEvent(mint: string) {
 	const router = useRouter()
 
-	const handleSwapEvent = useEventCallback((event: Event) => {
-		console.log('Swap Event:', event)
-		// Handle the event data as needed
-		if (event?.mint.toBase58() === mint) {
-			router.refresh()
-		}
-	})
+	const _mint = useMemo(() => new PublicKey(mint), [mint])
 
 	useEffect(() => {
-		// Add the event listener using the stable handler
-		const eventListenerId = program.addEventListener(
-			'swapEvent',
-			handleSwapEvent,
+		const subscriptionId = connection.onAccountChange(_mint, () =>
+			router.refresh(),
 		)
-
-		return () => {
-			program.removeEventListener(eventListenerId)
-		}
-	}, [handleSwapEvent])
+	}, [router, _mint])
 }
